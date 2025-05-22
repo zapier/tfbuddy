18297177dc224673045e0cbcc84953c36661dafd
Adam Pietrzycki <adam.pietrzycki@zapier.com>
2025-05-22 17:48:54 +0100

    clean up dead code
    Signed-off-by: Adam Pietrzycki <adam.pietrzycki@zapier.com>

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -34,17 +34,6 @@ func GetLastTag(dir string) string {
 	return CleanTagReference(GetLastTagRef(dir))
 }
 
-// clean the head ref
-func cleanHeadReference(tagRef *plumbing.Reference) string {
-	return cleanHeadRefName(tagRef.Strings()[0])
-}
-
-func cleanHeadRefName(refName string) string {
-	output := strings.Replace(refName, "refs/tags/", "", 1)
-	return output
-}
-
-// clean the tag ref
 func CleanTagReference(tagRef *plumbing.Reference) string {
 	return CleanTagRefName(tagRef.Strings()[0])
 }
@@ -52,109 +41,6 @@ func CleanTagReference(tagRef *plumbing.Reference) string {
 func CleanTagRefName(refName string) string {
 	output := strings.Replace(refName, "refs/tags/", "", 1)
 	return output
-}
-
-func CheckoutHead(dir string, branch string) error {
-	r, _, err := openRepo(dir)
-	if err != nil {
-		return err
-	}
-
-	log.Debug().Msg("checking out HEAD")
-	ref, err := r.Head()
-	if err != nil {
-		return err
-	}
-
-	return CheckoutRef(dir, ref)
-}
-
-func CheckoutTag(dir string, tag string) error {
-	r, _, err := openRepo(dir)
-	if err != nil {
-		return err
-	}
-
-	log.Debug().Msgf("checking out tag: %s", tag)
-	ref, err := r.Tag(tag)
-	if err != nil {
-		return err
-	}
-	return CheckoutRef(dir, ref)
-}
-
-func CheckoutRef(dir string, ref *plumbing.Reference) error {
-	_, w, err := openRepo(dir)
-	if err != nil {
-		return err
-	}
-
-	status, err := w.Status()
-	if err != nil {
-		return err
-	}
-	if !status.IsClean() {
-		log.Fatal().Msgf("module directory has uncommited changes, cannot checkout ref: %s", ref)
-	}
-
-	log.Debug().Msgf("checking out: %s", FormatRef(ref))
-
-	err = w.Checkout(&git.CheckoutOptions{
-		Hash: ref.Hash(),
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func CheckoutRefName(dir string, ref *plumbing.Reference) error {
-	_, w, err := openRepo(dir)
-	if err != nil {
-		return err
-	}
-
-	status, err := w.Status()
-	if err != nil {
-		return err
-	}
-	if !status.IsClean() {
-		log.Fatal().Msgf("module directory has uncommited changes, cannot checkout ref: %s", ref)
-	}
-
-	log.Debug().Msgf("checking out: %s", FormatRef(ref))
-
-	err = w.Checkout(&git.CheckoutOptions{
-		Branch: ref.Name(),
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GetHeadCommit(dir string) (ref *plumbing.Reference, err error) {
-	r, _, err := openRepo(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	return r.Head()
-}
-
-func openRepo(dir string) (*git.Repository, *git.Worktree, error) {
-	r, err := git.PlainOpen(dir)
-	if err != nil {
-		return nil, nil, err
-	}
-	w, err := r.Worktree()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return r, w, nil
 }
 
 func FormatRef(ref *plumbing.Reference) string {
