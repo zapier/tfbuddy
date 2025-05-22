5e7e4d0c7768cefacf5b548566fc9933d6b2ec85
Adam Pietrzycki <adam.pietrzycki@zapier.com>
2025-05-22 18:34:16 +0100

    improve clone depth handling of cases and add tests
    Signed-off-by: Adam Pietrzycki <adam.pietrzycki@zapier.com>

--- pkg/git/git_repo.go
+++ pkg/git/git_repo.go
@@ -3,6 +3,7 @@ package git
 import (
 	"fmt"
 	"io/fs"
+	"math"
 	"os"
 	"strconv"
 
@@ -27,9 +28,11 @@ func NewRepository(repo *git.Repository, auth *githttp.BasicAuth, localDir strin
 		Repository:     repo,
 	}
 }
+
 func (gr *Repository) GetLocalDirectory() string {
 	return gr.localDir
 }
+
 func (gr *Repository) FetchUpstreamBranch(branch string) error {
 	ref := fmt.Sprintf("refs/heads/%s:refs/heads/%s", branch, branch)
 	err := gr.Fetch(&git.FetchOptions{
@@ -41,6 +44,7 @@ func (gr *Repository) FetchUpstreamBranch(branch string) error {
 	}
 	return nil
 }
+
 func (gr *Repository) GetMergeBase(oldest, newest string) (string, error) {
 	var hashes []*plumbing.Hash
 	for _, rev := range []string{oldest, newest} {
@@ -50,6 +54,7 @@ func (gr *Repository) GetMergeBase(oldest, newest string) (string, error) {
 		}
 		hashes = append(hashes, hash)
 	}
+
 	var commits []*object.Commit
 	for _, hash := range hashes {
 		commit, err := gr.CommitObject(*hash)
@@ -58,45 +63,50 @@ func (gr *Repository) GetMergeBase(oldest, newest string) (string, error) {
 		}
 		commits = append(commits, commit)
 	}
+
 	res, err := commits[0].MergeBase(commits[1])
 	if err != nil {
 		return "", utils.CreatePermanentError(err)
 	}
-
 	if len(res) > 0 {
 		println(res)
 		return res[0].Hash.String(), nil
 
 	}
+
 	return "", utils.CreatePermanentError(fmt.Errorf("could not find merge base"))
 }
 func (gr *Repository) GetModifiedFileNamesBetweenCommits(oldest, newest string) ([]string, error) {
-
 	oldestSha, err := gr.ResolveRevision(plumbing.Revision(oldest))
 	if err != nil {
 		return nil, utils.CreatePermanentError(err)
 	}
+
 	newestSha, err := gr.ResolveRevision(plumbing.Revision(newest))
 	if err != nil {
 		return nil, utils.CreatePermanentError(err)
 	}
+
 	oldestCommit, err := gr.CommitObject(*oldestSha)
 	if err != nil {
 		return nil, utils.CreatePermanentError(err)
 	}
+
 	newestCommit, err := gr.CommitObject(*newestSha)
 	if err != nil {
 		return nil, utils.CreatePermanentError(err)
 	}
+
 	patch, err := oldestCommit.Patch(newestCommit)
 	if err != nil {
 		return nil, utils.CreatePermanentError(err)
 	}
-	filePatches := patch.FilePatches()
 
+	filePatches := patch.FilePatches()
 	if len(filePatches) == 0 {
 		return []string{}, nil
 	}
+
 	output := make([]string, 0, len(filePatches))
 	for _, file := range filePatches {
 		f, t := file.Files()
@@ -115,6 +125,7 @@ func (gr *Repository) GetModifiedFileNamesBetweenCommits(oldest, newest string) 
 
 	return output, nil
 }
+
 func WalkRepo(s string, d fs.DirEntry, err error) error {
 	if err != nil {
 		return err
@@ -126,13 +137,24 @@ func WalkRepo(s string, d fs.DirEntry, err error) error {
 }
 
 // GetCloneDepth reads the provided env var and returns an int to be used as git clone depth. Default is 0.
-func GetCloneDepth(envVar string) int {
+func GetCloneDepth(envVar string) (int, error) {
 	val := os.Getenv(envVar)
-	if val != "" {
-		depth, err := strconv.Atoi(val)
-		if err == nil {
-			return depth
-		}
+	if val == "" {
+		return 0, nil
 	}
-	return 0
+
+	depth, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, fmt.Errorf("invalid %s value '%s': must be a valid integer", envVar, val)
+	}
+
+	if depth < 0 {
+		return 0, fmt.Errorf("invalid %s value %d: clone depth cannot be negative", envVar, depth)
+	}
+
+	if depth > math.MaxInt32 {
+		return 0, fmt.Errorf("invalid %s value %d: clone depth cannot exceed %d", envVar, depth, math.MaxInt32)
+	}
+
+	return depth, nil
 }
--- pkg/git/git_repo_test.go
+++ pkg/git/git_repo_test.go
@@ -1,9 +1,13 @@
 package git
 
 import (
+	"io/fs"
+	"math"
 	"os"
+	"path/filepath"
 	"testing"
 
+	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/assert"
 	"github.com/zapier/tfbuddy/pkg/mocks"
 )
@@ -94,39 +98,279 @@ func TestGetModifiedFileNamesBetweenCommitsNoResults(t *testing.T) {
 	assert.Equal(t, len(modifiedFiles), 0, "expected no files modified between master and test")
 }
 
-func TestGitCloneDepth(t *testing.T) {
-	testVar := "git-clone-test"
+func TestGetCloneDepth(t *testing.T) {
+	const testEnvVar = "TEST_GIT_CLONE_DEPTH"
 
-	originalValue, originalExists := os.LookupEnv(testVar)
+	originalValue, originalExists := os.LookupEnv(testEnvVar)
 	t.Cleanup(func() {
 		if originalExists {
-			os.Setenv(testVar, originalValue)
+			os.Setenv(testEnvVar, originalValue)
 		} else {
-			os.Unsetenv(testVar)
+			os.Unsetenv(testEnvVar)
 		}
 	})
 
-	t.Run("no value", func(t *testing.T) {
-		err := os.Unsetenv(testVar)
-		if err != nil {
-			t.Fatalf("Failed to unset env var %s: %v", testVar, err)
-		}
-		assert.Equal(t, 0, GetCloneDepth(testVar))
+	tests := []struct {
+		name        string
+		envVal      string
+		wantDepth   int
+		wantErr     bool
+		errContains string
+	}{
+		{
+			name:      "no value",
+			envVal:    "",
+			wantDepth: 0,
+			wantErr:   false,
+		},
+		{
+			name:      "valid depth 500",
+			envVal:    "500",
+			wantDepth: 500,
+			wantErr:   false,
+		},
+		{
+			name:        "invalid string",
+			envVal:      "somestuff",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "must be a valid integer",
+		},
+		{
+			name:      "zero value",
+			envVal:    "0",
+			wantDepth: 0,
+			wantErr:   false,
+		},
+		{
+			name:        "negative value",
+			envVal:      "-10",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "cannot be negative",
+		},
+		{
+			name:      "large valid value",
+			envVal:    "999999",
+			wantDepth: 999999,
+			wantErr:   false,
+		},
+		{
+			name:      "max int32 value",
+			envVal:    "2147483647",
+			wantDepth: math.MaxInt32,
+			wantErr:   false,
+		},
+		{
+			name:        "over max value",
+			envVal:      "2147483648",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "cannot exceed",
+		},
+		{
+			name:        "very large value",
+			envVal:      "999999999999",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "cannot exceed",
+		},
+		{
+			name:      "minimum shallow clone",
+			envVal:    "1",
+			wantDepth: 1,
+			wantErr:   false,
+		},
+		{
+			name:        "float value",
+			envVal:      "10.5",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "must be a valid integer",
+		},
+		{
+			name:        "hex value",
+			envVal:      "0x10",
+			wantDepth:   0,
+			wantErr:     true,
+			errContains: "must be a valid integer",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.envVal == "" {
+				os.Unsetenv(testEnvVar)
+			} else {
+				os.Setenv(testEnvVar, tt.envVal)
+			}
+
+			t.Cleanup(func() {
+				os.Unsetenv(testEnvVar)
+			})
+
+			gotDepth, gotErr := GetCloneDepth(testEnvVar)
+
+			if tt.wantErr {
+				if gotErr == nil {
+					t.Errorf("GetCloneDepth() error = nil, wantErr %v", tt.wantErr)
+					return
+				}
+				if tt.errContains != "" {
+					assert.Contains(t, gotErr.Error(), tt.errContains)
+				}
+			} else {
+				if gotErr != nil {
+					t.Errorf("GetCloneDepth() error = %v, wantErr %v", gotErr, tt.wantErr)
+					return
+				}
+			}
+
+			if gotDepth != tt.wantDepth {
+				t.Errorf("GetCloneDepth() = %v, want %v", gotDepth, tt.wantDepth)
+			}
+		})
+	}
+}
+
+func TestNewRepository(t *testing.T) {
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	auth := &githttp.BasicAuth{
+		Username: "test",
+		Password: "token",
+	}
+	localDir := "/tmp/test"
+
+	repo := NewRepository(gitRepo.Repo, auth, localDir)
+
+	assert.NotNil(t, repo)
+	assert.Equal(t, auth, repo.authentication)
+	assert.Equal(t, localDir, repo.localDir)
+	assert.Equal(t, gitRepo.Repo, repo.Repository)
+}
+
+func TestGetLocalDirectory(t *testing.T) {
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	localDir := "/tmp/test"
+	repo := NewRepository(gitRepo.Repo, nil, localDir)
+
+	assert.Equal(t, localDir, repo.GetLocalDirectory())
+}
+
+func TestFetchUpstreamBranch(t *testing.T) {
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	repo := NewRepository(gitRepo.Repo, nil, "")
+
+	err := repo.FetchUpstreamBranch("master")
+	assert.NotNil(t, err, "should fail when no remote is configured")
+}
+
+func TestGetMergeBase_ErrorCases(t *testing.T) {
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	client := Repository{
+		Repository: gitRepo.Repo,
+	}
+
+	t.Run("invalid_oldest_branch", func(t *testing.T) {
+		_, err := client.GetMergeBase("nonexistent", "master")
+		assert.NotNil(t, err)
 	})
 
-	t.Run("500 depth", func(t *testing.T) {
-		err := os.Setenv(testVar, "500")
-		if err != nil {
-			t.Fatalf("Failed to set env var %s: %v", testVar, err)
-		}
-		assert.Equal(t, 500, GetCloneDepth(testVar))
+	t.Run("invalid_newest_branch", func(t *testing.T) {
+		_, err := client.GetMergeBase("master", "nonexistent")
+		assert.NotNil(t, err)
+	})
+}
+
+func TestGetModifiedFileNamesBetweenCommits_ErrorCases(t *testing.T) {
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	client := Repository{
+		Repository: gitRepo.Repo,
+	}
+
+	t.Run("invalid_oldest_commit", func(t *testing.T) {
+		_, err := client.GetModifiedFileNamesBetweenCommits("invalidhash", "master")
+		assert.NotNil(t, err)
 	})
 
-	t.Run("invalid value", func(t *testing.T) {
-		err := os.Setenv(testVar, "somestuff")
-		if err != nil {
-			t.Fatalf("Failed to set env var %s: %v", testVar, err)
-		}
-		assert.Equal(t, 0, GetCloneDepth(testVar))
+	t.Run("invalid_newest_commit", func(t *testing.T) {
+		_, err := client.GetModifiedFileNamesBetweenCommits("master", "invalidhash")
+		assert.NotNil(t, err)
+	})
+}
+
+func TestGetModifiedFileNamesBetweenCommits_FileOperations(t *testing.T) {
+	mrBranch := "test"
+	gitRepo, _ := mocks.InitGitTestRepo(t)
+	err := gitRepo.SwitchToBranch(mrBranch)
+	assert.Equal(t, nil, err)
+
+	commit1Hash, err := gitRepo.CreateCommitFileOnCurrentBranch("file1.tf", "content1")
+	assert.Equal(t, nil, err)
+
+	commit2Hash, err := gitRepo.CreateCommitFileOnCurrentBranch("file2.tf", "content2")
+	assert.Equal(t, nil, err)
+
+	commit3Hash, err := gitRepo.CreateCommitFileOnCurrentBranch("file3.tf", "content3")
+	assert.Equal(t, nil, err)
+
+	client := Repository{
+		Repository: gitRepo.Repo,
+	}
+
+	t.Run("added_files", func(t *testing.T) {
+		modifiedFiles, err := client.GetModifiedFileNamesBetweenCommits(commit1Hash, commit2Hash)
+		assert.Equal(t, nil, err)
+		assert.Contains(t, modifiedFiles, "file2.tf")
+	})
+
+	t.Run("multiple_files", func(t *testing.T) {
+		modifiedFiles, err := client.GetModifiedFileNamesBetweenCommits(commit2Hash, commit3Hash)
+		assert.Equal(t, nil, err)
+		assert.Contains(t, modifiedFiles, "file3.tf")
+	})
+}
+
+func TestWalkRepo(t *testing.T) {
+	tempDir := t.TempDir()
+
+	testFile := filepath.Join(tempDir, "test.txt")
+	err := os.WriteFile(testFile, []byte("test content"), 0644)
+	assert.NoError(t, err)
+
+	subDir := filepath.Join(tempDir, "subdir")
+	err = os.Mkdir(subDir, 0755)
+	assert.NoError(t, err)
+
+	subFile := filepath.Join(subDir, "subfile.txt")
+	err = os.WriteFile(subFile, []byte("sub content"), 0644)
+	assert.NoError(t, err)
+
+	t.Run("walk_file", func(t *testing.T) {
+		info, err := os.Stat(testFile)
+		assert.NoError(t, err)
+		dirEntry := fs.FileInfoToDirEntry(info)
+
+		err = WalkRepo(testFile, dirEntry, nil)
+		assert.NoError(t, err)
+	})
+
+	t.Run("walk_directory", func(t *testing.T) {
+		info, err := os.Stat(subDir)
+		assert.NoError(t, err)
+		dirEntry := fs.FileInfoToDirEntry(info)
+
+		err = WalkRepo(subDir, dirEntry, nil)
+		assert.NoError(t, err)
+	})
+
+	t.Run("walk_with_error", func(t *testing.T) {
+		info, err := os.Stat(testFile)
+		assert.NoError(t, err)
+		dirEntry := fs.FileInfoToDirEntry(info)
+
+		testErr := assert.AnError
+		err = WalkRepo(testFile, dirEntry, testErr)
+		assert.Equal(t, testErr, err)
 	})
 }
